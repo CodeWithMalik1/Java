382a52b4206358a252c2c0a2073e27ef6d1dc184
Mohammed Malik <malikmohammed2067@gmail.com>
2025-08-24 22:36:03 +0530

    Add files via upload

---
+++ BinaryTree/InOrder.java
@@ -0,0 +1,57 @@
+class InOrder{
+    static class Node{
+        int data;
+        Node left,right;
+        //Constructor
+        public Node(int item){
+            data=item;
+            left=right=null;
+
+        }
+
+    }
+
+
+    static class BinaryTree{
+        static int idx=-1;
+    public static Node buildTree(int nodes[]){
+        idx++;
+        if(nodes[idx]==-1){
+            return null;
+        }
+        Node newNode=new Node(nodes[idx]);
+        newNode.left=buildTree(nodes);
+        newNode.right=buildTree(nodes);
+        return newNode;         
+    }
+    // public static void Preorder(Node root){
+    //     if(root==null) return;
+    //     System.out.print(root.data+" ");
+    //     Preorder(root.left);
+    //     Preorder(root.right);
+    // }
+    // public static void postorder(Node root){
+    //     if(root==null) return;
+    //     postorder(root.left);
+    //     postorder(root.right);
+    //     System.out.print(root.data+" ");
+    // }
+    public static void Inorder(Node root){
+        if(root==null) return;
+        Inorder(root.left);
+        System.out.print(root.data+" ");
+        Inorder(root.right);
+
+    }
+
+    }
+    public static void main(String[] args){
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree=new BinaryTree();
+        Node root=tree.buildTree(nodes);
+        // tree.Preorder(root);
+        tree.Inorder(root);
+
+
+    }
+}
