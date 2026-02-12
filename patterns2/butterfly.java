0a7a1a3b8c13cd033ec37442f1d3c1e954ad0590
Mohammed Malik <malikmohammed2067@gmail.com>
2026-02-12 16:47:46 +0530

    Update butterfly.java

--- patterns2/butterfly.java
+++ patterns2/butterfly.java
@@ -1,4 +1,4 @@
-public class butterfly {
+public class Main{
     public static void butterflyPattern(int n) {
         // first half
         for (int i = 1; i <= n; i++) {
@@ -21,7 +21,7 @@ public class butterfly {
         }
 
         // second half
-        for (int i = n; i >= 1; i--) {
+        for (int i = n-1; i >= 1; i--) {
             // stars *
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
